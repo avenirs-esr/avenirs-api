55ae2c2570b0405d25cc5d1bbcaf56a548dc2ef3
Arnaud Deman <arnaud.deman@sppapps.com>
2024-02-09 11:39:07 +0100

    feat: audience in notification

--- src/main/java/fr/avenirsesr/avenirsapi/notification/model/Notification.java
+++ src/main/java/fr/avenirsesr/avenirsapi/notification/model/Notification.java
@@ -9,20 +9,33 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 /**
  * Representation of a Notification. This concerns the user notification as well as more internal ones
  * like System notification.
  */
+@Accessors(chain = true)
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+
 @Table(name="notification")
 public class Notification {
+	
+	/** Constant for the Broadcast audience */
+	final static String BROADCAST="*";
 	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	/** Audience of the notification */
+	private String audience;
 	
 	/** The priority of the notification. */
 	private NotificationPriority priority = NotificationPriority.MEDIUM;
@@ -41,22 +54,39 @@ public class Notification {
 	private String footer;
 
 	/**
-	 * Builds an instance of Notification.
-	 */
-	public Notification() {
-		
-	}
-	
-	/**
-	 * Builds an instance of Notification
+	 * Builds an instance of Notification for Broadcasting;
 	 * @param header The header of the notification
 	 * @param body The body (message) of the notification.
 	 * @param footer Footer off the notification.
 	 */
 	public Notification(String header, String body, String footer) {
+		this(BROADCAST, header, body, footer);
+	}
+	
+	/**
+	 * Builds an instance of Notification
+	 * @param audience The audience of the notification.
+	 * @param header The header of the notification
+	 * @param body The body (message) of the notification.
+	 * @param footer Footer off the notification.
+	 */
+	public Notification(String audience, String header, String body, String footer) {
+		this.audience = audience;
 		this.body = body;
 		this.header = header;
 		this.footer =  footer;
 	}
+	
+//	/**
+//	 * Builds an instance of Notification
+//	 * @param header The header of the notification
+//	 * @param body The body (message) of the notification.
+//	 * @param footer Footer off the notification.
+//	 */
+//	public Notification(String header, String body, String footer) {
+//		this.body = body;
+//		this.header = header;
+//		this.footer =  footer;
+//	}
 
 }
